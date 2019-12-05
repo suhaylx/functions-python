811917b3211531d4ab504fbed8a66ad7c5bf9410
suhaylx <akmsodikov@gmail.com>
2019-12-05 14:18:14 +0000

    paremeters vs paremeters

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#paremeters the values that fuction uses:yourname, emoji
 def its_hello_saying(yourname, emoji):
   print(f"Helloo {yourname}, you look {emoji}")
 
+#paremeters are values that you assign to functions : Himmy , Madara, 🤗
 its_hello_saying("Himmy", ":kissing_heart:🤗")
 its_hello_saying(emoji = " ‎😈",yourname = "Madara")
