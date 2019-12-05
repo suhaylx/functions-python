914b8d693807193683877b069930908dc7b26d93
suhaylx <akmsodikov@gmail.com>
2019-12-05 14:14:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+def its_hello_saying(yourname, emoji):
+  print(f"Helloo {yourname}, you look {emoji}")
+
+its_hello_saying("Himmy", ":kissing_heart:🤗")
+its_hello_saying(emoji = " ‎😈",yourname = "Madara")
